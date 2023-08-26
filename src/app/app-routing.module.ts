670113b753a5a58d7d806bc8ea11cc1053637f5c
Amine Mraihi <mraihiamin@gmail.com>
2023-08-26 09:07:20 +0100

    delete posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
-import { PostsComponent } from './modules/posts/posts.component';
 import { ControlDataComponent } from './modules/control-data/control-data.component';
 
 const routes: Routes = [{
